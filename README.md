c4ec20274c868688db96b603f59d67a9ff984895
Vinicius Pinheiro <viniciusps_1998@hotmail.com>
2023-01-04 11:28:24 -0300

    feat: structure

--- README.md
+++
@@ -1,1 +0,0 @@
-# patrocine-um-atleta
---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  root: {
+    --color-gray100: #f5f5f5;
+  }
+`;
+
+export default GlobalStyles;
