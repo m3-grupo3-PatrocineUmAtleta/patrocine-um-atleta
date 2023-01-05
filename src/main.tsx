3dbbdafd22bb4ce0ba6289ca11488d3f749427a4
Murilo Mendes <murilol147@gmail.com>
2023-01-05 15:35:16 -0300

    fix: GlobalStyles

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import App from "./App";
 import { Providers } from "./providers";
+import Fonts from "./styles/Fonts";
 import GlobalStyles from "./styles/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyles />
+      <Fonts />
       <Providers>
-        <GlobalStyles />
         <ToastContainer />
         <App />
       </Providers>
