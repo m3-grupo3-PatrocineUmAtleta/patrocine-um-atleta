0d2f32044390a3a5b1b5aeb9f13240b675c08e18
Anderson Keller <andersonkellervendas@hotmail.com>
2023-01-09 17:46:33 -0300

    fix: Container className and modal open Home

--- src/components/Modal/Modais/userLogOff/userLogoff.tsx
+++ src/components/Modal/Modais/userLogOff/userLogoff.tsx
@@ -25,12 +25,12 @@ export const UserLogoff = ({ idAthlete }: iUserLogoff) => {
           <p>
             {allAthletes
               .find((athlete) => athlete.id === idAthlete)
-              ?.bio.slice(0, 70)}
+              ?.bio?.slice(0, 70)}
           </p>
           <span className="blur body">
             {allAthletes
               .find((athlete) => athlete.id === idAthlete)
-              ?.bio.slice(70, -1)}
+              ?.bio?.slice(70, -1)}
           </span>
         </div>
         <Link to={"/login"}>Ver mais...</Link>
--- src/styles/Container.ts
+++ src/styles/Container.ts
@@ -20,7 +20,7 @@ export const StyledContainer = styled.div<iContainerProps>`
   @media (min-width: 750px) {
     width: 100%;
     max-width: 1285px;
-    div {
+    .rowReverse {
       display: flex;
       flex-direction: row-reverse;
     }
