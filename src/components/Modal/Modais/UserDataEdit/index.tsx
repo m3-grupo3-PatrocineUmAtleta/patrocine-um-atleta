4eab804944458f43589b531d0d522bacae81e41c
Julia Pereira <juliapt95@gmail.com>
2023-01-10 21:52:11 -0300

    "feat: modalEditUser"

--- src/components/Modal/Modais/UserDataEdit/index.tsx
+++ src/components/Modal/Modais/UserDataEdit/index.tsx
@@ -1,3 +1,143 @@
+import { Input, TextArea } from "../../../Form/Input";
+import { DivEditUser } from "./style";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface iFormDataEditUser {
+  name: string;
+  site?: string;
+  imgUrl?: string;
+  street: string;
+  city: string;
+  cep: string;
+  bio: string;
+  cnpj?: string;
+  cpf?: string;
+  phone: string;
+  email: string;
+}
+
 export const UserDataEdit = () => {
-  return <h1>User Edit</h1>;
+  const editUserSchema = yup.object().shape({
+    name: yup.string(),
+    site: yup.string(),
+    imgUrl: yup.string(),
+    street: yup.string(),
+    city: yup.string(),
+    cep: yup.string(),
+    bio: yup.string(),
+    cnpj: yup.string(),
+    cpf: yup.string(),
+    phone: yup.string(),
+    email: yup.string(),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid },
+  } = useForm<iFormDataEditUser>({
+    resolver: yupResolver(editUserSchema),
+  });
+
+  return (
+    <DivEditUser>
+      <form>
+        <div className="side-left">
+          <Input
+            id="name"
+            text="Nome da Instituição"
+            type="text"
+            placeholder="Razão Social"
+            register={register("name")}
+            message={errors.name?.message}
+          />
+          <Input
+            id="site"
+            text="Site"
+            type="text"
+            placeholder="Site"
+            register={register("site")}
+            message={errors.site?.message}
+          />
+          <Input
+            id="imagem"
+            text="Imagem"
+            type="text"
+            placeholder="Link da Imagem"
+            register={register("imgUrl")}
+            message={errors.imgUrl?.message}
+          />
+          <fieldset>
+            <legend>Endereço</legend>
+            <Input
+              id="street"
+              text="Rua"
+              type="text"
+              placeholder="Nome da rua "
+              register={register("street")}
+              message={errors.street?.message}
+            />
+            <Input
+              id="city"
+              text="Cidade - UF"
+              type="text"
+              placeholder="Cidade - UF "
+              register={register("city")}
+              message={errors.city?.message}
+            />
+            <Input
+              id="cep"
+              text="Cep"
+              type="text"
+              placeholder="00000-000"
+              register={register("cep")}
+              message={errors.cep?.message}
+            />
+          </fieldset>
+        </div>
+        <div className="side-right">
+          <TextArea
+            id="bio"
+            text="Biografia"
+            type="textarea"
+            placeholder="Biografia"
+            register={register("bio")}
+            message={errors.bio?.message}
+            rows={10}
+            height="160px"
+          />
+          <Input
+            id="cnpj"
+            text="CNPJ"
+            type="text"
+            placeholder="000.000.000-00"
+            register={register("cnpj")}
+            message={errors.cnpj?.message}
+          />
+          <fieldset>
+            <legend>Contatos</legend>
+            <Input
+              id="phone"
+              text="Telefone"
+              type="text"
+              placeholder="(00) 0000-0000"
+              register={register("phone")}
+              message={errors.phone?.message}
+            />
+            <Input
+              id="email"
+              text="Email"
+              type="text"
+              placeholder="seuemail@email.com"
+              register={register("email")}
+              message={errors.email?.message}
+            />
+          </fieldset>
+        </div>
+      </form>
+    </DivEditUser>
+  );
 };
--- src/components/Modal/Modais/UserDataEdit/style.ts
+++ src/components/Modal/Modais/UserDataEdit/style.ts
@@ -1,1 +1,30 @@
 import styled from "styled-components";
+
+export const DivEditUser = styled.div`
+
+  & > form {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 63px;
+
+    padding: 20px;
+    overflow-x:auto;
+    
+    & > div.side-left, & > div.side-right {
+      & > fieldset {
+        border: none;
+
+        & > label, legend,& > fieldset > label {
+          color: var(--color-primary-100)
+        }
+
+        & > textarea{
+          background-color: var(--color-primary-10);
+        }
+      & > input , & > fieldset > input{
+        background-color: var(--color-primary-10);
+      }
+    }   
+  }
+ 
+`;
