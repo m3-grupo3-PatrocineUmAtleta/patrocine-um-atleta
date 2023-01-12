4ad63e6efa21c1394eef538df930a5475e6dd734
Miguel <miguel.napolitano@live.com>
2023-01-11 22:27:32 -0300

    feat: creat a logic to new coment and avatar

---
+++ src/services/postComent.ts
@@ -0,0 +1,35 @@
+import { AxiosError } from "axios";
+import { ToastSucess, ToastError } from "../components/Toast";
+import { api } from "./api";
+
+export interface iDataDeposition {
+    athleteId: number;
+	content: string
+}
+
+interface iPostDepositionProps {
+    data: iDataDeposition;
+    userId: string | null;
+    localToken: string | null;
+}
+
+export const postDeposition = async ({
+    data, userId, localToken}: iPostDepositionProps) => {
+
+    try {
+      const response = await api.post(`/users/${userId}/deposition`, data, {
+      headers: {
+        authorization: `Bearer ${localToken}`,
+        "Content-Type": "application/json",
+      },      
+      })
+
+
+      ToastSucess("comentário feito! Obrigado!");
+
+    } catch (error) {
+      const err = error as AxiosError;
+      ToastError("Ops, " + err.response?.data);
+    } finally {
+    }
+  };
